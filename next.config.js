44eda81319def4a0752263e162bca21d6a1257de
hoangnhatfe <86102501+hoangnhatfe@users.noreply.github.com>
2023-10-04 18:00:05 +0700

    Add scss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter, JetBrains_Mono } from 'next/font/google'
 import Header from '@/components/Header'
